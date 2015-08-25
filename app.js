78e176897a8aed68205aa5b0536f12c8e01fb706
LeeJeongYeop <wjdduq914@gmail.com>
2015-08-25 13:46:13 +0900

    Session in Redis

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var log = require('./logger');
+var my = require('./routes/my_conf');
 
 /***********
  * My routes
@@ -15,8 +16,24 @@ var play = require('./routes/playCtrl');
 
 var session = require('express-session');  // express session
 
+var redis = require('redis'); //redis
+var RedisStore = require('connect-redis')(session);
+var client = redis.createClient();
+client.select(my.redisSelect());
+
 var app = express();
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUnitinialized: true,
+    store: new RedisStore({
+        host: my.redisAddr(),
+        port: my.redisPort(),
+        ttl: 60*60,
+        client: client
+    })
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
